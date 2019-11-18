1351b474995f3beea76bcd2ef50e97eef128a096
Ryan Adams <rwadams4th@gmail.com>
2019-11-17 20:01:07 -0600

    added another test

--- TestServerAdd.py
+++ TestServerAdd.py
@@ -7,7 +7,10 @@ from Server import *
 class TestServerAdd(unittest.TestCase):
 
 	def test_add(self):
-		self.assertTrue(Server("testHost", "testUser", "testPass", "testNick"))
+		self.assertTrue(Server("testHost", "testUser", "testPass", "testNick")) #test can add server
 
+	def test_missing_argument(self):
+		self.assertRaises(TypeError, Server, "host", "user", "pswd") #test if raise exception when missing argument
+		
 if __name__ == "__main__":
 	unittest.main() 
