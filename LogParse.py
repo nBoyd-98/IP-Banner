cf0d4b93252a45aa756fe39c9bb61e0ccb645bc5
Lucas Toole <lucas@ltoole.com>
2019-12-03 00:40:47 -0600

    Added Logic to read only from end of log file

--- LogParse.py
+++ LogParse.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import re
+import time
 import FirewallBan
 
 
@@ -9,19 +10,22 @@ import FirewallBan
 # should only process new lines written to the logfile, skipping the old lines.
 
 def logread(logfilepath):
-	with open(logfilepath) as logfile:
-		while True: # Run until program exits
-			line = logfile.readline()
-			if line:
-				match = re.search("sshd.*(Failed+|Unable+|not)", line)
-				if (match):
-					pull_ip(line)
-					
+        with open(logfilepath) as logfile:
+                logfile.seek(0,2)
+                while True: # Run until program exits
+                        line = logfile.readline()
+                        if line:
+                                match = re.search("sshd.*(Failed+|Unable+|not)", line)
+                                if (match):
+                                        pull_ip(line)
+                        else:
+                                time.sleep(0.1)
+                                continue
+
 				
 def pull_ip(line):
-	print(line)
 	ipaddr = re.search("((?:[0-9]{1,3}\.){3}[0-9]{1,3})", line).group(1)
-	print(ipaddr)
+	print("Banning " + ipaddr)
         FirewallBan.ban_ip(ipaddr)
 
 
